43a6e2306b1f2077ad5d7a0f26503e3d24854663
Kan O <zkan.cs@gmail.com>
2013-10-04 22:15:26 +0700

    add a demo script

---
+++ demo/demo.pl
@@ -0,0 +1,8 @@
+system( "../code/linreg ../data/input/training/L.txt ../data/input/testing/L.txt" );
+system( "../code/linreg ../data/input/training/L_after2weeks.txt ../data/input/testing/L_after2weeks.txt" );
+
+system( "../code/linreg ../data/input/training/a.txt ../data/input/testing/a.txt" );
+system( "../code/linreg ../data/input/training/a_after2weeks.txt ../data/input/testing/a_after2weeks.txt" );
+
+system( "../code/linreg ../data/input/training/b.txt ../data/input/testing/b.txt" );
+system( "../code/linreg ../data/input/training/b_after2weeks.txt ../data/input/testing/b_after2weeks.txt" );
