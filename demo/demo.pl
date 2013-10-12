557ec68c32b4fefc9881b3f970e3facb8b145809
Kan O <zkan.cs@gmail.com>
2013-10-12 13:55:14 +0700

    udpate demo script

--- demo/demo.pl
+++ demo/demo.pl
@@ -1,8 +1,4 @@
-system( "../code/linreg ../data/input/training/L.txt ../data/input/testing/L.txt" );
-system( "../code/linreg ../data/input/training/L_after2weeks.txt ../data/input/testing/L_after2weeks.txt" );
+chdir( "../code/" );
+system( "make" );
+system( "../code/tooth_shade_predictor" );
 
-system( "../code/linreg ../data/input/training/a.txt ../data/input/testing/a.txt" );
-system( "../code/linreg ../data/input/training/a_after2weeks.txt ../data/input/testing/a_after2weeks.txt" );
-
-system( "../code/linreg ../data/input/training/b.txt ../data/input/testing/b.txt" );
-system( "../code/linreg ../data/input/training/b_after2weeks.txt ../data/input/testing/b_after2weeks.txt" );
