66b8a7df40bfcbc1f9acd65144cfcf43b9d6c2ce
prontokan <kan@prontomarketing.com>
2014-04-02 21:47:08 +0700

    add more correlation analysis using SLR

--- code/tooth-shade-analysis.R
+++ code/tooth-shade-analysis.R
@@ -1,6 +1,9 @@
 # http://ww2.coastal.edu/kingw/statistics/R-tutorials/simplelinear.html
 
 data <- read.delim("~/Projects/tooth-shade-predictor/data/input/40cases.txt", header=F)
+#data <- read.delim("~/Projects/tooth-shade-predictor/data/input/30cases.txt", header=F)
+#data <- read.delim("~/Projects/tooth-shade-predictor/data/input/10cases.txt", header=F)
+
 plot(data$V1, data$V4)
 plot(data$V2, data$V4)
 plot(data$V3, data$V4)
@@ -24,4 +27,16 @@ abline(lm.out, col="red")
 lm.out = lm(data$V4 ~ data$V3)
 summary(lm.out)
 plot(data$V4 ~ data$V3)
+abline(lm.out, col="red")
+
+# SLR for L and a
+lm.out = lm(data$V1 ~ data$V2)
+summary(lm.out)
+plot(data$V1 ~ data$V2)
+abline(lm.out, col="red")
+
+# SLR for L and b
+lm.out = lm(data$V1 ~ data$V3)
+summary(lm.out)
+plot(data$V1 ~ data$V3)
 abline(lm.out, col="red")
