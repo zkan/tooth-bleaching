b16ec94257a75ada558a74135b555c5b3884bd53
prontokan <kan@prontomarketing.com>
2014-04-02 21:34:03 +0700

    add R code with simple linear regression

---
+++ code/tooth-shade-analysis.R
@@ -0,0 +1,27 @@
+# http://ww2.coastal.edu/kingw/statistics/R-tutorials/simplelinear.html
+
+data <- read.delim("~/Projects/tooth-shade-predictor/data/input/40cases.txt", header=F)
+plot(data$V1, data$V4)
+plot(data$V2, data$V4)
+plot(data$V3, data$V4)
+
+plot(data$V2, data$V1)
+plot(data$V3, data$V1)
+
+# SLR for DE and L
+lm.out = lm(data$V4 ~ data$V1)
+summary(lm.out)
+plot(data$V4 ~ data$V1)
+abline(lm.out, col="red")
+
+# SLR for DE and a
+lm.out = lm(data$V4 ~ data$V2)
+summary(lm.out)
+plot(data$V4 ~ data$V2)
+abline(lm.out, col="red")
+
+# SLR for DE and b
+lm.out = lm(data$V4 ~ data$V3)
+summary(lm.out)
+plot(data$V4 ~ data$V3)
+abline(lm.out, col="red")
