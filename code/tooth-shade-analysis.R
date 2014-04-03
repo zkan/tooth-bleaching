12e6e6690e81455b47ed528779041680975e252c
prontokan <kan@prontomarketing.com>
2014-04-03 22:14:41 +0700

    calculate MLR and output the results

--- code/tooth-shade-analysis.R
+++ code/tooth-shade-analysis.R
@@ -1,8 +1,6 @@
 # http://ww2.coastal.edu/kingw/statistics/R-tutorials/simplelinear.html
 
 data <- read.delim("~/Projects/tooth-shade-predictor/data/input/40cases.txt", header=F)
-#data <- read.delim("~/Projects/tooth-shade-predictor/data/input/30cases.txt", header=F)
-#data <- read.delim("~/Projects/tooth-shade-predictor/data/input/10cases.txt", header=F)
 
 plot(data$V1, data$V4)
 plot(data$V2, data$V4)
@@ -44,4 +42,19 @@ lm.out = lm(data$V1 ~ data$V3)
 lm.out
 summary(lm.out)
 plot(data$V1 ~ data$V3, xlab="b", ylab="L")
+abline(lm.out, col="red")
+
+# Multiple Linear Regression
+train <- read.delim("~/Projects/tooth-shade-predictor/data/input/30cases.txt", header=F)
+
+model = lm(train$V4 ~ train$V1 + train$V2 + train$V3)
+model
+summary(model)
+
+# dE between obtained dE and predicted dE
+dE <- read.delim("~/Projects/tooth-shade-predictor/data/input/mlr-results.txt", header=F)
+lm.out = lm(dE$V2 ~ dE$V1)
+lm.out
+summary(lm.out)
+plot(dE$V2 ~ dE$V1, xlab=expression("Predicted " * Delta * "E"), ylab=expression("Obtained " * Delta * "E"))
 abline(lm.out, col="red")
