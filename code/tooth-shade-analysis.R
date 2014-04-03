7d6e3f3dae19c76ace4368ce7010a3a864336234
prontokan <kan@prontomarketing.com>
2014-04-03 21:30:01 +0700

    add xlabel and ylabel + print out the coefficients

--- code/tooth-shade-analysis.R
+++ code/tooth-shade-analysis.R
@@ -13,30 +13,35 @@ plot(data$V3, data$V1)
 
 # SLR for DE and L
 lm.out = lm(data$V4 ~ data$V1)
+lm.out
 summary(lm.out)
-plot(data$V4 ~ data$V1)
+plot(data$V4 ~ data$V1, xlab="L", ylab=expression(Delta * "E"))
 abline(lm.out, col="red")
 
 # SLR for DE and a
 lm.out = lm(data$V4 ~ data$V2)
+lm.out
 summary(lm.out)
-plot(data$V4 ~ data$V2)
+plot(data$V4 ~ data$V2, xlab="a", ylab=expression(Delta * "E"))
 abline(lm.out, col="red")
 
 # SLR for DE and b
 lm.out = lm(data$V4 ~ data$V3)
+lm.out
 summary(lm.out)
-plot(data$V4 ~ data$V3)
+plot(data$V4 ~ data$V3, xlab="b", ylab=expression(Delta * "E"))
 abline(lm.out, col="red")
 
 # SLR for L and a
 lm.out = lm(data$V1 ~ data$V2)
+lm.out
 summary(lm.out)
-plot(data$V1 ~ data$V2)
+plot(data$V1 ~ data$V2, xlab="a", ylab="L")
 abline(lm.out, col="red")
 
 # SLR for L and b
 lm.out = lm(data$V1 ~ data$V3)
+lm.out
 summary(lm.out)
-plot(data$V1 ~ data$V3)
+plot(data$V1 ~ data$V3, xlab="b", ylab="L")
 abline(lm.out, col="red")
